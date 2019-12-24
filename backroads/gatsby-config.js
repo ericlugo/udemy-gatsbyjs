085a9069a903a7ed53320f1eb2f8ac0f101185a2
ericlugo <ericlugo@users.noreply.github.com>
2019-12-24 02:40:15 -0500

    updated dependencies. more work inbound.

--- backroads/gatsby-config.js
+++ backroads/gatsby-config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  plugins: [`gatsby-plugin-sass`, `gatsby-plugin-styled-components`],
+  siteMetadata: {
+    title: "Backroads",
+    description:
+      "Explore awesome worldwide tours & discover what makes each of them unique. Forget your daily routine & say 'yes' to adventure!",
+    author: "@johndoe",
+  },
+  plugins: [
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
 }
