25ccea547485295f02974b7c24a4c217aaa8c2b4
ericlugo <ericlugo@users.noreply.github.com>
2020-01-01 04:10:36 -0500

    facebook card added

--- backroads/src/components/SEO.js
+++ backroads/src/components/SEO.js
@@ -33,6 +33,14 @@ const SEO = ({ title, description }) => {
     <Helmet htmlAttributes={{ lang: "en" }} title={fullTitle}>
       <meta name="description" content={description || siteDesc} />
       <meta name="image" content={image} />
+      {/* facebook card */}
+      <meta property="og:url" content={siteURL} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={siteTitle} />
+      <meta property="og:description" content={description || siteDesc} />
+      <meta property="og:image" content={`${siteURL}${image}`} />
+      <meta property="og:image:width" content="400" />
+      <meta property="og:image:height" content="300" />
       {/* twitter card */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:creator" content={twitter} />
